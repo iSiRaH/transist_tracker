1e0ce5dbd3d7a697b61d7a4aafd8aac3bebb5f39
Isira Hansaja <153202522+iSiRaH@users.noreply.github.com>
2025-05-02 17:35:25 +0530

    Added page navigation

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:transist_tracker/pages/favourites_page.dart';
+import 'package:transist_tracker/pages/home_page.dart';
+import 'package:transist_tracker/pages/profile_page.dart';
+import 'package:transist_tracker/pages/search_page.dart';
+import 'package:transist_tracker/pages/time_table_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,8 +17,66 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+  int _currentIndex = 2;
+
+  final List<Widget> _pages = [
+    TimeTablePage(),
+    SearchPage(),
+    HomePage(),
+    FavouritesPage(),
+    ProfilePage(),
+  ];
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return MaterialApp(
+      title: "Transist tracker",
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        bottomNavigationBar: BottomNavigationBar(
+          currentIndex: _currentIndex,
+          selectedItemColor: Colors.blue,
+          unselectedItemColor: Colors.black,
+          onTap: (value) {
+            setState(() {
+              _currentIndex = value;
+            });
+          },
+          items: [
+            BottomNavigationBarItem(
+              icon: Icon(
+                Icons.schedule,
+              ),
+              label: "Time Table",
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(
+                Icons.search,
+              ),
+              label: "Search",
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(
+                Icons.map,
+              ),
+              label: "Map",
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(
+                Icons.favorite,
+              ),
+              label: "Favourites",
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(
+                Icons.person,
+              ),
+              label: "Profile",
+            ),
+          ],
+        ),
+        body: _pages[_currentIndex],
+      ),
+    );
   }
 }
