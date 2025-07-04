f238834a09949dda4ddbd166530325297591615c
HasalKenula <hasalkenula255@gmail.com>
2025-07-04 22:19:54 +0530

    added some icons to card

--- lib/pages/bus_card.dart
+++ lib/pages/bus_card.dart
@@ -1,66 +1,4 @@
-
-
 import 'package:flutter/material.dart';
-
-// class BusCard extends StatelessWidget {
-//   final String name;
-//   final String type;
-//   final String time;
-//   final String price;
-//   final String duration;
-//   final int seatsLeft;
-//   final Color seatColor;
-
-//   const BusCard({
-//     super.key,
-//     required this.name,
-//     required this.type,
-//     required this.time,
-//     required this.price,
-//     required this.duration,
-//     required this.seatsLeft,
-//     required this.seatColor,
-//   });
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Container(
-//       margin: const EdgeInsets.only(bottom: 14),
-//       padding: const EdgeInsets.all(14),
-//       decoration: BoxDecoration(
-//         color: Colors.white,
-//         borderRadius: BorderRadius.circular(18),
-//         boxShadow: const [
-//           BoxShadow(color: Colors.black12, blurRadius: 6, offset: Offset(0, 2)),
-//         ],
-//       ),
-//       child: Column(
-//         crossAxisAlignment: CrossAxisAlignment.start,
-//         children: [
-//           Text(name, style: const TextStyle(fontSize: 16, fontWeight: FontWeight.bold)),
-//           Text(type, style: TextStyle(color: Colors.grey[700])),
-//           const SizedBox(height: 6),
-//           Row(
-//             children: [
-//               Text(time, style: TextStyle(color: Colors.grey[800])),
-//               const Spacer(),
-//               Text(price,
-//                   style: const TextStyle(
-//                       fontSize: 16, fontWeight: FontWeight.bold, color: Colors.orange)),
-//               const SizedBox(width: 8),
-//               Text(duration, style: TextStyle(color: Colors.grey[600])),
-//             ],
-//           ),
-//           const SizedBox(height: 6),
-//           Text(
-//             "$seatsLeft Seats left",
-//             style: TextStyle(color: seatColor, fontWeight: FontWeight.bold),
-//           ),
-//         ],
-//       ),
-//     );
-//   }
-// }
 
 
 class BusCard extends StatelessWidget {
@@ -120,7 +58,24 @@ class BusCard extends StatelessWidget {
           const SizedBox(height: 6),
           Text(time, style:TextStyle(fontSize: 16, color:Colors.black, fontWeight: FontWeight.bold)),
           const SizedBox(height: 6),
-          Text("$seatsLeft seatsLeft", style:TextStyle(fontSize: 16, color: seatColor )),
+
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text("$seatsLeft seatsLeft", style:TextStyle(fontSize: 16, color: seatColor )),
+
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Icon(Icons.center_focus_strong), 
+                  Icon(Icons.history),            
+                  Icon(Icons.code),               
+                  Icon(Icons.logout),  
+                ],
+              )
+            ],
+          )
+         
           
         ],
       ),
